9aad74f2999d6b43cc063d183f72aa484e4cdbd0
Pascal Hartig <passy@twitter.com>
2014-09-08 20:34:00 +0200

    Learned about update SStatus, no need for manual OAuth juggling

--- Main.hs
+++ Main.hs
@@ -8,32 +8,25 @@ import qualified Data.Conduit.List as CL
 import qualified Data.Configurator as Conf
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import qualified Data.Text.Encoding as TE
 
 import Common (runTwitterFromEnv')
-import Control.Monad.Catch (MonadThrow)
-import Control.Monad.Trans.Resource (ResourceT)
 import Control.Monad.IO.Class (liftIO)
 import Control.Lens
-import Data.ByteString (ByteString)
-import Data.Conduit (ResumableSource)
-import Network.HTTP.Conduit (parseUrl, withManager, http, urlEncodedBody, Request, Response)
 import System.Environment (getArgs)
-import Web.Authenticate.OAuth (OAuth(..), Credential, signOAuth)
-import Web.Twitter.Conduit (stream, userstream, statusesFilterByTrack)
+import Web.Twitter.Conduit (stream, statusesFilterByTrack)
 import Web.Twitter.Types (StreamingAPI(..))
 import Web.Twitter.Types.Lens (AsStatus(..), userScreenName)
-
-statusesUrl :: String
-statusesUrl = "https://api.twitter.com/1.1/statuses/update.json"
 
 main :: IO ()
 main = do
     [confFile] <- getArgs
+    -- TODO: Instead of passing around conf, could
+    -- I wrap all this in a ReaderT?
     conf <- Conf.load [Conf.Required confFile]
+    username <- Conf.lookupDefault "" conf "userName"
 
     runTwitterFromEnv' conf $ do
-        src <- stream $ statusesFilterByTrack "@steampain"
+        src <- stream $ statusesFilterByTrack username
         src C.$$+- CL.mapM_ (^! act (liftIO . printTL))
 
     return ()
@@ -49,33 +42,3 @@ showStatus s = T.concat [ s ^. user . userScreenName
                         , ":"
                         , s ^. text
                         ]
-
-postTweet ::
-    OAuth ->
-    Credential ->
-    T.Text ->
-    IO (Response (ResumableSource (ResourceT IO) ByteString))
-postTweet oauth cred tweet = do
-    let params = [("status", TE.encodeUtf8 tweet)]
-    req <- makeRequest statusesUrl params
-    -- Error handling?
-    executeOAuthRequest oauth cred req
-
-makeRequest ::
-    MonadThrow m =>
-    String ->
-    [(ByteString, ByteString)] ->
-    m Request
-makeRequest url params = do
-    req <- parseUrl url
-    return $ urlEncodedBody params req
-
-executeOAuthRequest ::
-    OAuth ->
-    Credential ->
-    Request ->
-    IO (Response (ResumableSource (ResourceT IO) ByteString))
-executeOAuthRequest oauth cred request =
-    withManager $ \manager -> do
-        signed <- signOAuth oauth cred request
-        http signed manager
