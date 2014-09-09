da32bdeddb1f79ab551d309cf4cc25e73c12818a
Pascal Hartig <passy@twitter.com>
2014-09-09 15:40:22 +0200

    Reply to tweets

--- Main.hs
+++ Main.hs
@@ -10,13 +10,14 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
 import Common (runTwitterFromEnv')
+-- import Latex (renderLaTeXToFile)
 import Control.Monad.IO.Class (liftIO, MonadIO(..))
 import Control.Monad.Trans.Resource (MonadResource)
 import Control.Monad.Logger (MonadLogger)
 import Control.Lens
 import System.Environment (getArgs)
-import Web.Twitter.Conduit (stream, statusesFilterByTrack, MediaData(..), updateWithMedia, call, TW)
-import Web.Twitter.Types (StreamingAPI(..))
+import Web.Twitter.Conduit (stream, statusesFilterByTrack, MediaData(..), updateWithMedia, call, TW, inReplyToStatusId, update)
+import Web.Twitter.Types (StreamingAPI(..), Status(..))
 import Web.Twitter.Types.Lens (AsStatus(..), userScreenName)
 
 main :: IO ()
@@ -24,28 +25,50 @@ main = do
     [confFile] <- getArgs
     -- TODO: Instead of passing around conf, could
     -- I wrap all this in a ReaderT?
+    -- UPDATE: Turns out TW is already a `type TW m = ReaderT TWEnv m`
+    -- Does that help me?
     conf <- Conf.load [Conf.Required confFile]
     username <- Conf.lookupDefault "" conf "userName"
 
     runTwitterFromEnv' conf $ do
         src <- stream $ statusesFilterByTrack username
-        src C.$$+- CL.mapM_ (^! act (liftIO . printTL))
+        src C.$$+- CL.mapM_ (^! act printTL)
 
     return ()
 
+-- TODO: That's not at all what it does now.
 printTL ::
+    (MonadLogger m, MonadResource m) =>
     StreamingAPI ->
-    IO ()
-printTL (SStatus s) = T.putStrLn $ showStatus s
-printTL _ = T.putStrLn "Other event"
+    TW m ()
+printTL (SStatus s) = do
+    liftIO $ T.putStrLn $ showStatus s
+    replyToStatus s
+printTL _ = liftIO $ T.putStrLn "Other event"
 
-showStatus :: AsStatus s => s -> T.Text
+showStatus ::
+    AsStatus s =>
+    s ->
+    T.Text
 showStatus s = T.concat [ s ^. user . userScreenName
                         , ":"
                         , s ^. text
                         ]
 
-updateStatusWithImage :: (MonadLogger m, MonadResource m) => String -> FilePath -> TW m ()
+replyToStatus ::
+    (MonadLogger m, MonadResource m) =>
+    Status ->
+    TW m ()
+replyToStatus s = do
+    -- TODO: Do something with res, don't return ()
+    _ <- call $ update (T.concat ["@", s ^. user . userScreenName, " Hello World"]) & inReplyToStatusId ?~ (statusId s)
+    return ()
+
+updateStatusWithImage ::
+    (MonadLogger m, MonadResource m) =>
+    String ->
+    FilePath ->
+    TW m ()
 updateStatusWithImage status filepath = do
     res <- call $ updateWithMedia (T.pack status) (MediaFromFile filepath)
     liftIO $ print res
