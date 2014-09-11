29f87a0795d1ff3a9b186ba6ee249e84a58e361c
Pascal Hartig <passy@twitter.com>
2014-09-11 22:25:37 +0200

    Shell quoting

--- tex2png.sh
+++ tex2png.sh
@@ -5,20 +5,20 @@ set -xe
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 TEX=$1
-PDF=$(basename -s .tex $1).pdf
-PNG=$(basename -s .tex $1).png
+PDF=$(basename -s .tex "$1").pdf
+PNG=$(basename -s .tex "$1").png
 
-TDIR=`mktemp -d`
+TDIR=$(mktemp -d)
 
-# trap "{ cd - ; rm -rf $TDIR; exit 255; }" SIGINT
+trap "{ cd - ; rm -rf $TDIR; exit 255; }" SIGINT
 
-cd $TDIR
-    pdflatex $SCRIPTPATH/$TEX
-    pdfcrop $PDF $PDF
-    pdftocairo -singlefile -png $PDF
+cd "$TDIR"
+    pdflatex "$SCRIPTPATH/$TEX"
+    pdfcrop "$PDF" "$PDF"
+    pdftocairo -singlefile -png "$PDF"
 cd -
 
-mv $TDIR/$PNG .
-rm -rf $TDIR
+mv "$TDIR/$PNG" .
+rm -rf "$TDIR"
 
 exit 0
