20971ac927e7072f8f01fbd65cd5bf343a7dd52d
Pascal Hartig <passy@twitter.com>
2014-09-11 21:38:18 +0200

    Make tex2png more resiliant

--- tex2png.sh
+++ tex2png.sh
@@ -1,12 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 set -xe
 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 TEX=$1
 PDF=$(basename -s .tex $1).pdf
 PNG=$(basename -s .tex $1).png
 
-pdflatex $TEX
-pdfcrop $PDF $PDF
-pdftocairo -png $PDF
-echo $PNG
+TDIR=`mktemp -d`
+
+# trap "{ cd - ; rm -rf $TDIR; exit 255; }" SIGINT
+
+cd $TDIR
+    pdflatex $SCRIPTPATH/$TEX
+    pdfcrop $PDF $PDF
+    pdftocairo -singlefile -png $PDF
+cd -
+
+mv $TDIR/$PNG .
+rm -rf $TDIR
+
+exit 0
