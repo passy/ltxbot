6ecbf7ae22669e479b0868d92600886964dfab8c
Pascal Hartig <passy@twitter.com>
2014-09-21 17:03:58 +0100

    Read UID from file

--- Main.hs
+++ Main.hs
@@ -3,17 +3,19 @@ module Main where
 
 import Prelude
 
-import Web.Twitter.LtxBot
+import Control.Lens
+import Control.Monad (liftM, when)
+import Data.Maybe (listToMaybe, isNothing, fromJust)
+import System.Environment (getArgs)
+import Web.Twitter.Types (UserId)
+import Web.Twitter.Conduit (stream, statusesFilterByTrack)
+import Web.Twitter.LtxBot (actTL, normalizeMentions)
 import Web.Twitter.LtxBot.Common (runTwitterFromEnv')
 
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
 import qualified Data.Configurator as Conf
 import qualified Data.Text as T
-
-import Control.Lens
-import System.Environment (getArgs)
-import Web.Twitter.Conduit (stream, statusesFilterByTrack)
 
 main :: IO ()
 main = do
@@ -25,9 +27,13 @@ main = do
     conf <- Conf.load [Conf.Required confFile]
     username <- Conf.lookupDefault "" conf "userName"
 
+    maybeUid <- liftM (listToMaybe . T.split (== '-')) (Conf.lookupDefault "" conf "accessToken")
+    let userId = fmap (read . T.unpack) maybeUid :: Maybe UserId
+    when (isNothing userId) $ error "accessToken must contain a '-'"
+
     runTwitterFromEnv' conf $ do
         src <- stream $ statusesFilterByTrack $ T.concat ["@", username]
         -- TODO: Add conduit that removes mentions
-        src C.$=+ normalizeMentions C.$$+- CL.mapM_ (^! act actTL)
+        src C.$$+- CL.mapM_ (^! act (actTL $ fromJust userId))
 
     return ()
--- Web/Twitter/LtxBot.hs
+++ Web/Twitter/LtxBot.hs
@@ -4,14 +4,11 @@ module Web.Twitter.LtxBot where
 import Prelude
 
 import qualified Data.Conduit as C
-import qualified Data.Conduit.List as CL
-import qualified Data.Configurator as Conf
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Web.Twitter.Types.Lens as TL
 import qualified Web.Twitter.Types as TT
 
-import Web.Twitter.LtxBot.Common (runTwitterFromEnv')
 import Web.Twitter.LtxBot.Latex (renderLaTeXToHandle, standaloneLaTeX)
 import Control.Monad (liftM, join)
 import Control.Monad.IO.Class (liftIO, MonadIO(..))
@@ -20,12 +17,11 @@ import Control.Monad.Logger (MonadLogger)
 import Control.Monad.Catch (MonadCatch, MonadMask)
 import Control.Lens
 import Control.Applicative ((<$>))
-import System.Environment (getArgs)
 import System.IO.Temp (withSystemTempFile)
 import System.FilePath (replaceExtension)
 import System.IO (hClose)
-import Web.Twitter.Conduit (stream, statusesFilterByTrack, MediaData(..), updateWithMedia, call, TW, inReplyToStatusId)
-import Web.Twitter.Types (StreamingAPI(..), Status(..))
+import Web.Twitter.Conduit (MediaData(..), updateWithMedia, call, TW, inReplyToStatusId)
+import Web.Twitter.Types (StreamingAPI(..), Status(..), UserId)
 import System.Process (system)
 import Data.Maybe (maybeToList)
 
@@ -34,24 +30,26 @@ normalizeMentions = C.await >>= maybe (return ()) C.yield
 
 actTL ::
     (MonadLogger m, MonadResource m, MonadCatch m, MonadMask m) =>
+    UserId ->
     StreamingAPI ->
     TW m ()
-actTL (SStatus s) = actStatus s
-actTL _ = liftIO $ T.putStrLn "Other event"
+actTL u (SStatus s) = actStatus u s
+actTL _ _ = liftIO $ T.putStrLn "Other event"
 
 actStatus :: (MonadLogger m, MonadResource m, MonadCatch m, MonadMask m) =>
+    UserId ->
     Status ->
     TW m ()
-actStatus s = do
+actStatus uid s = do
     liftIO $ T.putStrLn $ showStatus s
     withSystemTempFile "hatmp.tex" (\ tmpFile tmpHandle -> do
-        -- Yuck, this is state, global state even. Let's figure out
-        -- if this can be piped through stdin.
+        -- Yuck, this is mutable state, global mutable state even. Let's figure
+        -- out if this can be piped through stdin.
         _ <- liftIO $ do
             renderLaTeXToHandle tmpHandle (standaloneLaTeX (s ^. TL.text))
             hClose tmpHandle -- We can't write to the handle otherwise
             system $ unwords ["./docker-tex2png.sh", tmpFile]
-        replyStatusWithImage s (replaceExtension tmpFile ".png"))
+        replyStatusWithImage uid s (replaceExtension tmpFile ".png"))
 
 showStatus ::
     TL.AsStatus s =>
@@ -64,19 +62,18 @@ showStatus s = T.concat [ s ^. TL.user . TL.userScreenName
 
 replyStatusWithImage ::
     (MonadLogger m, MonadResource m) =>
+    UserId ->
     Status ->
     FilePath ->
     TW m ()
-replyStatusWithImage status filepath = do
+replyStatusWithImage uid status filepath = do
     -- TODO: Do something with res, don't return ()
     res <- call updateCall
     liftIO $ print res
     return ()
     where
         allMentions = extractStatusMentions status
-        -- TODO: Nopedeynope. Let's read the ID from TW as it's part of the
-        -- accessToken
-        otherMentions = filter (\u -> TT.userEntityUserId u /= (2810142194 :: TT.UserId)) allMentions
+        otherMentions = filter (\u -> TT.userEntityUserId u /= uid) allMentions
         mentionsString = T.unwords $ (\m -> T.concat ["@", m ^. TL.userEntityUserScreenName]) <$> otherMentions
         statusString = T.unwords [T.concat ["@", status ^. TL.user . TL.userScreenName], mentionsString]
         media = MediaFromFile filepath
@@ -91,4 +88,3 @@ extractStatusMentions s = do
     let ues = s ^. TL.statusEntities >>= (^? TL.enUserMentions)
     let mentions = liftM (fmap (^. TL.entityBody)) ues
     join $ maybeToList mentions
-
