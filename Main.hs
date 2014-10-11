f7e8e7903570b26af8802f3f559fde4390366986
Pascal Hartig <passy@twitter.com>
2014-10-11 12:38:55 +0100

    Add proper CLI interface
    Help text, version output etc.

--- Main.hs
+++ Main.hs
@@ -1,25 +1,49 @@
-{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts, DeriveDataTypeable #-}
 module Main where
 
 import Prelude
 
 import Control.Lens
 import Control.Monad (liftM, when)
+import Data.Data (Data)
 import Data.Maybe (listToMaybe, isNothing, fromJust)
-import System.Environment (getArgs)
-import Web.Twitter.Types (UserId)
+import Data.Typeable (Typeable)
+import Data.Version (showVersion)
+import Paths_ltxbot (version)
+import System.Console.CmdArgs.Explicit (HelpFormat(..), helpText)
 import Web.Twitter.Conduit (stream, statusesFilterByTrack)
 import Web.Twitter.LtxBot (actTL, normalizeMentions)
 import Web.Twitter.LtxBot.Common (runTwitterFromEnv')
+import Web.Twitter.Types (UserId)
 
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
 import qualified Data.Configurator as Conf
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified System.Console.CmdArgs.Implicit as CA
+
+data Ltxbot = Ltxbot { config :: FilePath }
+    deriving (Show, Data, Typeable)
+
+programName :: String
+programName = "ltxbot"
+
+args :: CA.Mode (CA.CmdArgs Ltxbot)
+args = CA.cmdArgsMode $ Ltxbot { config = CA.def CA.&= CA.args }
+    CA.&= CA.summary (unwords [programName, showVersion version])
+    CA.&= CA.program programName
 
 main :: IO ()
 main = do
-    [confFile] <- getArgs
+    mainArgs <- CA.cmdArgsRun args
+    let confFile = config mainArgs
+    if null confFile
+        then print $ helpText [] HelpFormatDefault args
+        else runBot confFile
+
+runBot :: FilePath -> IO ()
+runBot confFile = do
     -- TODO: Instead of passing around conf, could
     -- I wrap all this in a ReaderT?
     -- UPDATE: Turns out TW is already a `type TW m = ReaderT TWEnv m`
@@ -31,6 +55,7 @@ main = do
     let userId = fmap (read . T.unpack) maybeUid :: Maybe UserId
     when (isNothing userId) $ error "accessToken must contain a '-'"
 
+    T.putStrLn $ T.unwords ["Listening for Tweets to", username, "…"]
     runTwitterFromEnv' conf $ do
         src <- stream $ statusesFilterByTrack $ T.concat ["@", username]
         src C.$=+ normalizeMentions C.$$+- CL.mapM_ (^! act (actTL $ fromJust userId))
